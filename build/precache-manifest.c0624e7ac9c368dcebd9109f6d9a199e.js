29fea781d2a1dd298b9eaf31606a4a9cddf45475
Samuel Cooper <cooper.samuel@outlook.com>
2019-01-03 16:30:27 -0500

    added built files

---
+++ build/precache-manifest.c0624e7ac9c368dcebd9109f6d9a199e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "88356f81126017233df1",
+    "url": "./static/js/main.88356f81.chunk.js"
+  },
+  {
+    "revision": "2d1bf5b7d4211839c17c",
+    "url": "./static/js/1.2d1bf5b7.chunk.js"
+  },
+  {
+    "revision": "88356f81126017233df1",
+    "url": "./static/css/main.f9450af4.chunk.css"
+  },
+  {
+    "revision": "9ca3bf820d106ad2cc76514e108b7a30",
+    "url": "./index.html"
+  }
+];
